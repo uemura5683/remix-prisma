ff4bf64512004fb2a057c8fb62f932a27f405a99
uemura5683 <nobuyoshi5683@gmail.com>
2024-08-05 00:39:26 +0900

    jsonを修正

--- remix-prisma-json-main/app/routes/_index.tsx
+++ remix-prisma-json-main/app/routes/_index.tsx
@@ -4,36 +4,41 @@ import { useLoaderData } from "@remix-run/react";
 import { prisma } from "~/db.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const array: any = [];
+
   const url = new URL(request.url);
   const searchParams = url.searchParams;
   const id = searchParams.get('id');
   const password = searchParams.get('password');
-
   const todos = await prisma.idpass.findMany();
   const exists = todos.some((todo: any) => todo.id == id && todo.password == password);
 
   var error = '';
   switch(true) {
     case id === '' && password === '' && exists === false || id === null && password == null && exists === false:
-      var error = 'IDとパスワードを入力してください';
+      var error = "IDとパスワードを入力してください";
       break;
     case id === '' && exists === false:
-      var error = 'IDを入力してください';
+      var error = "IDを入力してください";
       break;
     case password === '' && exists === false:
-      var error = 'パスワードを入力してください';      
+      var error = "パスワードを入力してください";      
       break;
     case id !== '' && password !== '' && exists === false:
-      var error = 'IDとPasswordが一致しません';
+      var error = "IDとPasswordが一致しません";
       break;
     default: 
-      var error = '';
+      var error = "";
     break;
   }
 
   // jsonはRemixが用意しているヘルパー関数
   // JSONレスポンスを返す
-  return json({ exists, 'error': error });
+
+  array.push({ exists, 'error': error })
+
+  return array;
+
 };
 
 export default function Index() {
