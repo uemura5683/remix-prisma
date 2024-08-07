853279a1f0a8530994333866ab23dca19fa301da
uemura5683 <nobuyoshi5683@gmail.com>
2024-08-08 00:09:47 +0900

    prismaを修正

--- remix-prisma-json-main/app/routes/_index.tsx
+++ remix-prisma-json-main/app/routes/_index.tsx
@@ -1,7 +1,6 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { prisma } from "~/db.server";
-
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const array: any = [];
   const url = new URL(request.url);
@@ -10,9 +9,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   const password = searchParams.get('password');
   const todos = await prisma.idpass.findMany();
   const exists = todos.some((todo: any) => todo.id == id && todo.password == password);
-
   var error: string = '';
-
   switch(true) {
     case id === '' && password === '' && exists === false || id === null && password == null && exists === false:
       var error = "IDとパスワードを入力してください";
@@ -32,9 +29,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   }
   array.push({ exists, 'error': error })
   return array;
-
 };
-
 export default function Index() {
   const data = useLoaderData<typeof loader>();
   return (
--- remix-prisma-json-main/app/routes/json.tsx
+++ remix-prisma-json-main/app/routes/json.tsx
@@ -1,20 +1,10 @@
-import { json } from "@remix-run/node";
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { prisma } from "~/db.server";
-
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const url = new URL(request.url);
-  const searchParams = url.searchParams;
-  const id = searchParams.get('id');
-  const password = searchParams.get('password');
-
   const todos = await prisma.idpass.findMany();
-  // jsonはRemixが用意しているヘルパー関数
-  // JSONレスポンスを返す
-  return json({ todos });
+  return todos;
 };
-
 export default function Index() {
   const todos = useLoaderData<typeof loader>();
   return (
